3bbc8d53506d40576d525f4bc60f78757e63429b
gabrielpedrosaf <gabriel.pedrosaf@gmail.com>
2021-11-02 02:11:10 -0300

    Enviando pagina sobre

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 function Home(){
     return (
         <div>
             <h2> Home 2</h2>
+            <Link href="/sobre">
+            <a>Sobre</a>
+            </Link>
         </div>
         )
 }
---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+function Sobre(){
+    return (
+        <div>
+            <h2> Sobre 2</h2>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+        </div>
+        )
+}
+
+export default Sobre;
