7b4c4fa79e5706491cb75cec43c7832e30f9ed7e
gabrielpedrosa <gabriel.pedrosaf@gmail.com>
2021-11-17 14:14:45 -0300

    teste com dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import PageContent from './pagecontent.js';
+import Dashboard from '../components/dashboard';
 
 
 function Home(){
@@ -8,6 +9,7 @@ function Home(){
         <>
             <PageContent>
                 <Navbar/>
+                <Dashboard/>
                 <Footer/>
             </PageContent>
         </>
